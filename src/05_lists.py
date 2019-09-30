5f862a645774113a048065f340589d2a20cd05f4
maxgunter99 <maxgunter99@gmail.com>
2019-09-30 15:41:28 -0500

    added lists.py

--- src/05_lists.py
+++ src/05_lists.py
@@ -1,3 +1,5 @@
+import os
+os.system( 'clear' )
 # For the exercise, look up the methods and functions that are available for use
 # with Python lists.
 
@@ -8,22 +10,36 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE 
+
+x.append( 4 )
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE 
+
+for i in range( len(y) ):
+    x.append( y[i] )
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE 
+
+x.remove( 8 )
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE 
+
+#      index , value
+x.insert( 5 , 99 )
 print(x)
 
 # Print the length of list x
 # YOUR CODE HERE 
+print( len( x ) )
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+
+for i in range( len( x ) ):
+    print( x[i] * 1000 )
