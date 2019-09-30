96e6d34e2fecabd6588729c216c39a13a7fdf28e
maxgunter99 <maxgunter99@gmail.com>
2019-09-30 16:45:06 -0500

    added slices and comprehensions

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -1,3 +1,5 @@
+import os
+os.system( 'clear' )
 """
 List comprehensions are one cool and unique feature of Python.
 They essentially act as a terse and concise way of initializing
@@ -12,12 +14,18 @@ for more info regarding list comprehensions.
 
 y = []
 
+for i in range( 5 ):
+    y.append( i + 1 )
+
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
 y = []
+
+for i in range( 10 ):
+    y.append( i * i * i )
 
 print(y)
 
@@ -28,6 +36,9 @@ a = ["foo", "bar", "baz"]
 
 y = []
 
+for i in range( len( a ) ):
+    y.append( a[i].upper() )
+
 print(y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
@@ -37,5 +48,12 @@ x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
 y = []
+
+for i in range( len( x ) ):
+    if ( int( x[i]) % 2 == 0 ):
+        print( f' {x[i]} is even! 😄' )
+        y.append( x[i] )
+    else:
+        print( f' {x[i]} is odd! ☹️' )
 
 print(y)
