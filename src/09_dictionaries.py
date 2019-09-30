c9bc31d67aa295fc38812edb981deae2726f8454
maxgunter99 <maxgunter99@gmail.com>
2019-09-30 16:56:42 -0500

    added dictionaries.py

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -1,3 +1,5 @@
+import os
+os.system( 'clear' )
 """
 Dictionaries are Python's implementation of associative arrays.
 There's not much different with Python's version compared to what
@@ -36,9 +38,28 @@ waypoints = [
 # Add a new waypoint to the list
 # YOUR CODE HERE
 
+waypoints.append(
+
+    {
+        "lat": 00,
+        "lon": 00,
+        "name": 'Zero Zero'
+    }
+
+)
+
+print( waypoints )
+
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # YOUR CODE HERE
 
+waypoints[0].update({ 'name': 'not a real place' })
+
+print( waypoints )
+
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+
+for i in range( len( waypoints ) ):
+    print( waypoints[i].values() )
