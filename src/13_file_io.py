0220d3aa4032cdaa659e2c7f36838c8321ff5cf0
maxgunter99 <maxgunter99@gmail.com>
2019-10-01 15:49:03 -0500

    added fileio.py

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -1,3 +1,5 @@
+import os
+os.system( 'clear' )
 """
 Python makes performing file I/O simple. Take a look
 at how to read and write to files here: 
@@ -9,6 +11,13 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+def openFile():
+    x = open( 'foo.txt' , 'rt' )
+    print( x.read() )
+    x.close()
+
+openFile()
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +25,15 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+def writeFile():
+    x = open( 'bar.txt' , 'w' )
+    x = open( 'bar.txt' , 'a' )
+    x.write( ' Hey there.' )
+    x.write( ' This is a file now.' )
+    x.write( ' Bye.' )
+    x = open( 'bar.txt' , 'rt' )
+    print( x.read() )
+    x.close()
+
+
+writeFile()
