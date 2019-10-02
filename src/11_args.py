3ade6b3f79c08cc65dab69daba7923abebc5f1a9
maxgunter99 <maxgunter99@gmail.com>
2019-10-01 19:09:19 -0500

    added classes

--- src/11_args.py
+++ src/11_args.py
@@ -19,13 +19,13 @@ print(f1(1, 2))
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
-def f2( *argv ):
+def f2( *args ):
 
     x = 0
 
-    for i in range( len( argv ) ):
-        x += argv[i]
-    return( x )
+    for i in range( len( args ) ):
+        x += args[i]
+    return x
 
 # YOUR CODE HERE
 
@@ -36,10 +36,10 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
 a = [7, 6, 5, 4]
 
-# ________________TODO________________________________________
 # What thing do you have to add to make this work?
-# print( f2( a ) )    # Should print 22
-# ____________________________________________________________
+# print( a )
+# print( type( a ) )
+print( f2( *a ) )    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -98,4 +98,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4( **d )
